215f2f661ecaa84bff91d71bb44beee50aad4ae4
Jackson_J <1556837534@qq.com>
2019-01-10 22:35:31 +0800

    MapReduce 使用Combiner 重写 WordCount程序

---
+++ src/main/java/com/BigData/MapReduce/Demo/EMPTotalSalesMapReduce/combiner/CombinerWordCountMain.java
@@ -0,0 +1,50 @@
+package com.BigData.MapReduce.Demo.EMPTotalSalesMapReduce.combiner;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+/**
+ * @BelongsProject: BigDataPro
+ * @BelongsPackage: com.bigData.MapReduce
+ * @Author: 15568
+ * @CreateTime: 2018-12-28 22:23
+ * @Description: 5. 主程序
+ * idea 打包为 可执行Jar   打包执行的代码不能放在  test 目录下
+ * https://blog.csdn.net/u011499747/article/details/78970070
+ */
+public class CombinerWordCountMain {
+    public static void main(String[] args) throws Exception {
+        // 1.创建一个 job = Mapper + Reduce
+        Job job = Job.getInstance(new Configuration());
+
+        // 指定Job 的入口
+        job.setJarByClass(CombinerWordCountMain.class);
+
+        // 指定任务的 Mapper 和输出的数据类型
+        job.setMapperClass(CombinerWordCountMapper.class);
+        job.setMapOutputKeyClass(Text.class);    // key2 数据类型
+        job.setMapOutputValueClass(LongWritable.class); // value2 数据类型
+
+        // 设置 任务的 Combiner Combiner是一种特殊的Reducer
+        job.setCombinerClass(CombinerWordCountReducer.class);
+
+        //指定任务的 Reducer
+        job.setReducerClass(CombinerWordCountReducer.class);
+        job.setOutputKeyClass(Text.class);    //key4 类型
+        job.setOutputValueClass(LongWritable.class); // value4 类型
+
+        //指定输入数据  输出数据
+        FileInputFormat.setInputPaths(job,new Path(args[0]));
+        FileOutputFormat.setOutputPath(job,new Path(args[1]));
+
+        // 执行任务
+        job.waitForCompletion(true);
+
+        // 2. 导出为Jar 包 放到 Hadoop 的 Yarn 容器中运行
+    }
+}
---
+++ src/main/java/com/BigData/MapReduce/Demo/EMPTotalSalesMapReduce/combiner/CombinerWordCountReducer.java
@@ -0,0 +1,37 @@
+package com.BigData.MapReduce.Demo.EMPTotalSalesMapReduce.combiner;
+
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+import java.io.IOException;
+
+/**
+ * @BelongsProject: BigDataPro
+ * @BelongsPackage: com.bigData.MapReduce
+ * @Author: 15568
+ * @CreateTime: 2018-12-28 22:23
+ * @Description: 3. 单词汇总
+ *      需要继承 Reducer 父类 传入 Hadoop 中的数据类型  即 Reducer 接收的 key-value 类型 与输出的 key-value 类型
+ */
+public class CombinerWordCountReducer extends Reducer<Text, LongWritable,Text,LongWritable> {
+    /**
+     * 4. 重写 reduce 方法 就是对Map 拆分后的任务进行汇总
+     * @param key3
+     * @param values3  汇总后存放是上文 Map 中 同一个单词的出现次数 比如 I （1,1）
+     * @param context  Reducer 上下文  上文是 Mapper 下文 是HDFS
+     * @throws IOException
+     * @throws InterruptedException
+     */
+    @Override
+    protected void reduce(Text key3, Iterable<LongWritable> values3, Context context) throws IOException, InterruptedException {
+        // 进行任务循环汇总
+        long total = 0l;
+        for (LongWritable l : values3) {
+            total += l.get();
+        }
+
+        // 输出 k4 v4
+        context.write(key3,new LongWritable(total));
+    }
+}
